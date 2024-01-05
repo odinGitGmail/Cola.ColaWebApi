ad59fd10d8ff87c65dd7c41131a28ebb0d22702f
Din Ya <>
2024-01-05 16:39:40 +0800

    fix: change method name

--- OdinBadRequest.cs
+++ OdinBadRequest.cs
@@ -1,4 +1,4 @@
-﻿using Cola.Core.Utils.Extensions;
+﻿using Cola.CoreUtils.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cola.ColaWebApi;
@@ -7,6 +7,6 @@ public class OdinBadRequest : BadRequestObjectResult
 {
     public OdinBadRequest(string errorCode, string message) : base(new ErrorModel(errorCode, message))
     {
-        StatusCode = errorCode.ToInt();
+        StatusCode = errorCode.StringToInt();
     }
 }
